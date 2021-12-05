31e193d8f2e81941902bac460f7446ea855c124b
froprintoai <froprintoai@gmail.com>
2021-12-05 04:14:17 -0600

    before debugging

--- src/components/Layout/EditMenu.js
+++ src/components/Layout/EditMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect, useState } from 'react/cjs/react.development';
+import { useEffect, useState} from 'react';
 
 import Storage from '@aws-amplify/storage';
 import API from '@aws-amplify/api';
